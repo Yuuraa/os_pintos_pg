b150f8f09c0eb5113056b38232dd3ec9eb2c30d7
yyjjjj <lyj1107@gist.ac.kr>
2020-06-07 21:29:19 +0900

    Done argument passing, echo x

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -1,20 +1,45 @@
 #include "userprog/syscall.h"
+
 #include <stdio.h>
 #include <syscall-nr.h>
+
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
-static void syscall_handler (struct intr_frame *);
+static void syscall_handler(struct intr_frame *);
+void exit(int);
+int write(int, const void *, unsigned);
 
-void
-syscall_init (void) 
-{
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+void syscall_init(void) {
+    intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
-{
-  printf ("system call!\n");
-  thread_exit ();
+syscall_handler(struct intr_frame *f) {
+    printf("system call! : %d\n", *(uint32_t *)(f->esp));
+    switch (*(uint32_t *)(f->esp)) {
+        case SYS_HALT:
+            break;
+        case SYS_EXIT:
+            exit(*(uint32_t *)(f->esp + 4));
+            break;
+        case SYS_WRITE:
+            write((int)*(uint32_t *)(f->esp + 4), (void *)*(uint32_t *)(f->esp + 8), (unsigned)*((uint32_t *)(f->esp + 12)));
+            break;
+    }
+
+    thread_exit();
+}
+
+void exit(int status) {
+    printf("%s: exit(%d)\n", thread_current()->name, status);
+    thread_exit();
+}
+
+int write(int fd, const void *buffer, unsigned size) {
+    if (fd == 1) {
+        putbuf(buffer, size);
+        return size;
+    }
+    return -1;
 }
--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -1,6 +1,8 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-void syscall_init (void);
+void syscall_init(void);
+void exit(int);  // add syscall exit
+int write(int, const void *, unsigned);
 
 #endif /* userprog/syscall.h */
